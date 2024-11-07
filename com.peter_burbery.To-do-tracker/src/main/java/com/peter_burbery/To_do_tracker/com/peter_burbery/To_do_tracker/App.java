fe1bc5fe0c04c1cafd1232c44fcff1a60b92bb31
Peter Cullen Burbery <peter.cullen.burbery@gmail.com>
2024-11-06 21:40:40 -0500

    add
    track other things

---
+++ com.peter_burbery.To-do-tracker/src/main/java/com/peter_burbery/To_do_tracker/com/peter_burbery/To_do_tracker/App.java
@@ -0,0 +1,10 @@
+package com.peter_burbery.To_do_tracker.com.peter_burbery.To_do_tracker;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ com.peter_burbery.To-do-tracker/src/test/java/com/peter_burbery/To_do_tracker/com/peter_burbery/To_do_tracker/AppTest.java
@@ -0,0 +1,19 @@
+package com.peter_burbery.To_do_tracker.com.peter_burbery.To_do_tracker;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
